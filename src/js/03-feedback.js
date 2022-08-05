c1e9bc59381ec09d3b26d710b7410cce870735d2
KsanaSal <oasalivon@gmail.com>
2022-08-06 00:18:58 +0300

    Solution task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const throttle = require('lodash.throttle');
 
+const feedbackForm = document.querySelector('.feedback-form');
+const input = document.querySelector("[name = email]");
+input.value = "fgh"
+const getForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+console.log(getForm);
+const form = {
+  email: '',
+  message: '',
+};
+feedbackForm.addEventListener(
+  'input',
+  throttle(evt => {
+    if (evt.target.name === 'email') {
+      form.email = evt.target.value;
+    } else if (evt.target.name === 'message') {
+      form.message = evt.target.value;
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(form));
+    console.log(form);
+    console.log(evt.target.name);
+    console.dir(evt.target);
+    console.dir(evt.currentTarget);
+  }, 500)
+);
+console.log('feedbackForm');
+
+feedbackForm.children[0].value = "hjh";
