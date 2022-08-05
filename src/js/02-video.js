f59e0f8ff30bd5fcfbf213b96c136fde34e8f88d
KsanaSal <oasalivon@gmail.com>
2022-08-05 20:16:27 +0300

    Solution task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+const getVideoCurrentTime =
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) ?? 0;
+
+player
+  .setCurrentTime(getVideoCurrentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
